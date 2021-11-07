95743f84a37a7e5864b9213b237e56c2b8bff8af
codeLoverX <52366079+IAmWannaBeCoder@users.noreply.github.com>
2021-11-07 16:57:59 +0600

    am

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -77,4 +77,13 @@ const login = async (req, res, next) => {
   }
 };
 
-module.exports = { signup, login };
+const getUsers = async (req, res, next) => {
+  try {
+    const user = await User.find({ });
+    res.status(200).json({ user });
+  } catch (error) {
+    res.status(500).json({ error: err });
+  }
+};
+
+module.exports = { signup, login, getUsers };
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -3,6 +3,7 @@ const UserController = require("../controllers/UserController");
 const checkAuth = require("../middlewares/check-auth");
 const router = Router();
  
+router.get('/', UserController.getUsers)
 router.post("/signup", checkAuth, UserController.signup);
 router.post("/login", UserController.login);
 
